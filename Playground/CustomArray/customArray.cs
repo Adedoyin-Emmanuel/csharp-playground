95adc4bd59225d351e0361e73e75cbef1c436277
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-08 17:03:24 +0100

    feat: implemented custom array

---
+++ Playground/CustomArray/customArray.cs
@@ -0,0 +1,100 @@
+namespace Csharp_Playground.Playground.CustomArray;
+
+class CustomArray
+{
+
+    // insert
+    // removeAt
+    // indexOf
+
+    private int[] _items;
+    private int _size;
+    private int _capacity;
+
+    public CustomArray()
+    {
+        _items = new int[5];
+        _size = 0;
+        _capacity = 5;
+    }
+
+
+    public void Insert(int item)
+    {
+        if (_size > _capacity)
+        {
+            _capacity *= 2;
+
+            int[] newArray = new int[_capacity];
+
+            for (int i = 0; i < _size - 1; i++)
+            {
+                newArray[i] = _items[i];
+            }
+
+            _items = newArray;
+
+        }
+
+        _items[_size] = item;
+
+
+        _size++;
+    }
+
+    public void RemoveAt(int index)
+    {
+        // Validate index.
+
+        /*
+            Index must not be < 0 or > size - 1
+        */
+
+        if (index < 0 || index > _size - 1)
+        {
+            throw new Exception("Invalid index!");
+        }
+
+        for (int i = index; i < _size - 1; i++)
+        {
+            _items[i] = _items[i + 1];
+        }
+
+        _size--;
+
+    }
+
+    public int IndexOf(int value)
+    {
+
+        for (int i = 0; i < _size; i++)
+        {
+            if (_items[i] == value)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+    public int Count() 
+    {
+        return _size;
+    }
+
+
+    /*An array shouldn't know anything about printing to the console 
+      According to Software Design principles.
+      But for the sake of this practise, I will add it
+    */
+
+    public void Print()
+    {
+        foreach (var item in _items)
+        {
+            System.Console.WriteLine(item);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,100 +1,20 @@
-﻿class CustomArray {
+﻿using Csharp_Playground.Playground.CustomArray;
 
-    // insert
-    // removeAt
-    // indexOf
+namespace Csharp_Playground;
 
-    private int[] _items;
-    private int _size;
-    private int _capacity;
-
-    public CustomArray()
+class Program
+{
+    public static void Main(string[] args)
     {
-        _items = new int[5];
-        _size = 0;
-        _capacity = 5;
-    }
+        var numbers = new CustomArray();
 
+        numbers.Insert(1);
+        numbers.Insert(2);
+        numbers.Insert(3);
 
-    public void Insert(int item)
-    {
-        if(_size > _capacity)
-        {
-            _capacity *= 2;
+        numbers.RemoveAt(2);
+        numbers.Insert(3);
 
-            int[] newArray = new int[_capacity];
-
-            for (int i = 0; i < _size - 1; i++)
-            {
-                newArray[i] = _items[i];
-            }
-
-            _items = newArray;
-
-        }
-
-        _items[_size] = item;
-
-
-        _size++;
-    }
-
-    public void RemoveAt(int index)
-    {
-        // Validate index.
-
-        /*
-            Index must not be < 0 or > size - 1
-        */
-
-        if(index < 0 || index > _size - 1)
-        {
-            throw new Exception("Invalid index!");
-        }
-
-        for (int i = index; i < _size - 1; i++)
-        {
-            _items[i] = _items[i + 1];
-        }
-
-        _size --;
-
-    }
-
-    public int IndexOf(int value)
-    {
-        int foundElement = -1;
-
-        for (int i = 0; i < _size - 1; i++)
-        {
-            if(_items[i] == value)
-            {
-                return _items[i];
-            }
-        }
-
-        return foundElement;
-    }
-    
-}
-
-
-class Program {
-    public static void Main(string [] args)
-    {
-       
-        List<int> numbers = [1, 2, 3];
-
-        numbers.Add(4);
-
-        
-
-        foreach(var number in numbers)
-        {
-            System.Console.WriteLine(number);
-        }
-
-        System.Console.WriteLine($"The length of the List is {numbers.Count}");
-
+        System.Console.WriteLine(numbers.IndexOf(3));
     }
 }
