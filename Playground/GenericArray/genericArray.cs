9517954d02bedd3fd3c3173ddade3e4a3448e1c0
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-09 07:35:19 +0100

    feat: implemented generic array

---
+++ Playground/GenericArray/genericArray.cs
@@ -0,0 +1,94 @@
+namespace Csharp_Playground.Playground.GenericArray;
+
+class GenericArray<T> 
+{
+
+    private int _size;
+    private int _capacity;
+    private T[] _items;
+
+
+    public GenericArray()
+    {
+        _size = 0;
+        _capacity = 5;
+        _items = new T[5];
+    }
+
+
+    /*
+        Insert,
+        RemoveAt, 
+        IndexOf
+        Count
+        Print
+    */
+
+    public void Insert(T element)
+    {
+        if(_size > _capacity)
+        {
+            _capacity *= 2;
+
+            T[] newArray = new T[_capacity];
+
+            for (int i = 0; i < _size - 1; i++)
+            {
+                newArray[i] = _items[i];
+            }
+
+            _items = newArray;
+
+        }
+
+        _items[_size] = element;
+        _size++;
+    }
+
+
+    public void RemoveAt(int index)
+    {
+       
+       if(index < 0 || index > _size - 1)
+       {
+            throw new ArgumentOutOfRangeException("Invalid index!");
+        }
+
+        for (int i = index; i < _size - 1; i++)
+        {
+            _items[i] = _items[i + 1];
+        }
+
+        _size--;
+    }
+
+
+    public int IndexOf(T value)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            if(_items[i]!.Equals(value))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+    public int Count()
+    {
+        return _size;
+    }
+
+    public void Print()
+    {
+        foreach(T item in _items)
+        {
+            System.Console.WriteLine(item);
+        }
+    }
+
+
+}
