581c3ea1bc6e2fabfe7a0dba345e700aac2b9ee4
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-10 22:12:06 +0100

    chore: playing with linked lists

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Collections.Generic;
 using Csharp_Playground.Playground.CustomArray;
 using Csharp_Playground.Playground.GenericArray;
 
@@ -8,19 +8,20 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var numbers = new CustomArray();
-
-        numbers.Insert(1);
-        numbers.Insert(2);
-        numbers.Insert(3);
-        numbers.Insert(4);
+        // var numbers = new CustomArray();
 
 
-        numbers.InsertAt(28, 0);
-        numbers.InsertAt(42, 2);
-        numbers.InsertAt(50, 0);
+        // numbers.Insert(1);
+        // numbers.Insert(2);
+        // numbers.Insert(3);
+        // numbers.Insert(4);
 
-        numbers.Print();
+
+        // numbers.InsertAt(28, 0);
+        // numbers.InsertAt(42, 2);
+        // numbers.InsertAt(50, 0);
+
+        // numbers.Print();
 
         // var reversedArray = numbers.Reverse();
 
@@ -83,9 +84,19 @@ class Program
 
         // nums.Print();
 
+        LinkedList<int> list = new LinkedList<int>();
 
+        list.AddLast(10);
+        list.AddLast(20);
+        list.AddLast(30);
+        
+        list.AddLast(40);
 
-
+        System.Console.WriteLine(list.Contains(10));
+        // foreach(var item in list)
+        // {
+        //     System.Console.WriteLine(item);
+        // }
     }
 
 }
